f0314c03cb44aaed65aa210f7f637226e3367151
cast.adree <andreacastrobonilla@gmail.com>
2022-12-05 09:02:03 +0000

    04/11/2022 - Activity2

--- app/src/main/java/com/example/aut2_02/Activity2_List.java
+++ app/src/main/java/com/example/aut2_02/Activity2_List.java
@@ -2,13 +2,57 @@ package com.example.aut2_02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Activity2_List extends AppCompatActivity {
+
+    private ListView lista;
+    private List<String> items;
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity2_list);
+
+        EditText txtNombre = findViewById(R.id.txtNombre);
+        EditText txtCantidad = findViewById(R.id.txtCantidad);
+        EditText txtPrecio = findViewById(R.id.txtPrecio);
+
+        Button btnAgregar = findViewById(R.id.btnAgregar);
+        Button btnEliminar = findViewById(R.id.btnEliminar);
+        lista = findViewById(R.id.lista);
+        items = new ArrayList<>();
+
+        adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1,items);
+        lista.setAdapter(adapter);
+
+        btnAgregar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String str = txtNombre.getText().toString() + " - " + txtCantidad.getText().toString() + "u. - " + txtPrecio.getText().toString() + "€";
+                items.add(str);
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        btnEliminar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                items.remove(txtNombre.getText().toString());
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/aut2_02/MainActivity.java
+++ app/src/main/java/com/example/aut2_02/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.aut2_02;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    GridView gridView;
     Button next;
 
     @Override
@@ -17,12 +27,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //MENÚ -> ACTIVIDAD 1
-        next=(Button) findViewById(R.id.productosButton);
+        next = (Button) findViewById(R.id.productosButton);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this,Activity1_Products.class);
                 startActivity(i);
+            }
+        });
+
+        //MENÚ -> ACTIVIDAD 2
+        next = (Button) findViewById(R.id.cestaButton);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,Activity2_List.class);
+                startActivity(i);
+            }
+        });
+
+        //DECLARAR E INSTANCIAR EL DIÁLOGO QUE EMERGERÁ AL PULSAR "ABOUT"
+        ImageButton about = (ImageButton) findViewById(R.id.aboutButton);
+
+        //FUNCIONALIDAD AL PULSAR EL BOTÓN
+        about.setOnClickListener(new AdapterView.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Andrea Castro Bonilla v1.0.0 2022©", Toast.LENGTH_LONG).show();
             }
         });
     }
